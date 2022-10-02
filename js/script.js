69b45f1be07398846bdbdb4d8e19d682452d372e
Krzysiek Jurewicz <k.jurewicz89@gmail.com>
2022-10-02 17:43:01 +0200

    add functions in JS and edit welcome message

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 {
     const welcome = () => {
         console.log("Witam wszystkich dewsów :P. Wiem że bez szału ale starałem się przećwiczyć chwytanie i podmianę elementu aby była robiona dobrze. Wiem że dużo linijek kodu jak na taką stronę ale z czasem mam nadzieję że będzie lepiej :p. Powoli ale skrupulatnie do przodu. Dodatkowo zmodyfikowałem trochę powitanie i starałem się commitować każdą zmianę. Proszę o konstruktywną krytykę i z góry wszystkim dziękuję. ");
-    
+
     }
     
-    let button = document.querySelector(".button");
-    let bigBoxOn = document.querySelector(".bigBoxOn");
-    let mainTitleOn = document.querySelector(".mainTitleOn");
-    let day = document.querySelector(".js-day");
-    let aimOn = document.querySelector(".aimOn");
-    let importantOn = document.querySelector(".importantOn")
+    const changeTheme = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", () => {
+            const day = document.querySelector(".js-day");
+            const text = document.querySelector(".js-button--text");
     
+            day.classList.toggle("night");
+            text.innerText = day.classList.contains("night") ? "Jasne" : "Ciemne"
+        });
+    }
     
-    button.addEventListener("click", () => {
-    
-        day.classList.toggle("night");
-        bigBoxOn.classList.toggle("bigBoxOff");
-        mainTitleOn.classList.toggle("mainTitleOff");
-        aimOn.classList.toggle("aimOff");
-        importantOn.classList.toggle("importantOff");
-    
-    });
+    const init = () => {
+        welcome();
+        changeTheme();
+    }
 
-    welcome();
+    init();
 }
